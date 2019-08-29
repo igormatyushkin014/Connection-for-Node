9fa42f12cb403f4eac6adea1b6807892bfe728c3
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-08-29 13:16:35 +0300

    Updated RAW types

--- src/io/raw/request.ts
+++ src/io/raw/request.ts
@@ -1,12 +1,12 @@
 export type Request = {
-	readonly type: "request",
+	readonly type: "raw.request",
 	readonly requestId: string,
 	readonly recipientId?: string,
 	readonly data: any
 };
 
 export const isRequest = (object: any): object is Request => {
-	return object.type === "request"
+	return object.type === "raw.request"
 		&& "requestId" in object
 		&& "data" in object;
 };
--- src/io/raw/response.ts
+++ src/io/raw/response.ts
@@ -1,10 +1,10 @@
 export type Response = {
-	readonly type: "response",
+	readonly type: "raw.response",
 	readonly requestId: string,
 	readonly data?: any
 };
 
 export const isResponse = (object: any): object is Response => {
-	return object.type === "response"
+	return object.type === "raw.response"
 		&& "requestId" in object;
 };
